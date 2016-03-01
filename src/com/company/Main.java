6565b6bc763a694c794cc0a8f681ae901bb9b5e4
wellingtonschu <wellington.schu@live.com>
2016-03-01 16:50:26 -0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Sou calouro burro");
+        System.out.println("Eu amo meus veteranos");
+
+    }
+}
